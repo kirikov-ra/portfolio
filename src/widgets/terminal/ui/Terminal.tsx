a2555a1e814d09b00e95558b505063329e751c64
Kirikov Roman <vetproginc@gmail.com>
2026-02-02 10:14:15 +0300

    feat(ui): add tailwind-scrollbar plugin and style terminal scrollbar

--- src/widgets/terminal/ui/Terminal.tsx
+++ src/widgets/terminal/ui/Terminal.tsx
@@ -1,13 +1,14 @@
 import Links from "./Links";
 import Skills from "./Skills";
 
-const Container = "bg-gray-300 rounded-[10px] p-4 flex-1 mt-4 overflow-y-auto";
+const UI = {
+    container: "bg-gray-300 rounded-[10px] p-4 flex-1 mt-4 overflow-y-auto scrollbar scrollbar-track-transparent scrollbar-thumb-white/10 scrollbar-hover:scrollbar-thumb-white/30 scrollbar-active:scrollbar-thumb-white/30"
+} as const;
 
 const Terminal = () => {
 
-
     return (
-        <div className={Container}>
+        <div className={UI.container}>
             <Skills />
             <Links />
         </div>
